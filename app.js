6e56a516545b52a39fc391372c8cc76fffc0a3c1
Robin <robin.noormets@voco.ee>
2022-09-20 11:19:25 +0300

    Create project files

---
+++ app.js
@@ -0,0 +1,23 @@
+// variables
+    // let
+    let name = "Robin Noormets"
+
+    // const
+    const surname = "Noormets"
+    // arrays
+    const numbers = [1, 2, 3, 4, 5]
+    console.log(numbers)
+    numbers.push(6)
+    console.log(numbers)
+
+    // objects
+const person = {
+    firstname: "Robin",
+    surname : "Noormets"
+}
+
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "IKT"
+console.log(person)
