86eadd3ace4120c6fe728ff75fea85a8a9454834
Robin <robin.noormets@voco.ee>
2022-09-22 13:45:08 +0300

    Condition statements

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
-const today = new Date()
+/*
+if(condition) {
 
-let val
+}
+else {
 
-val = today
-val = today.getMonth() // 0 - jaanuar
-val = today.getDate()
-val = today.getFullYear()
+}
 
-val = new Date("09/22/2021")
-val = new Date("September 23 2022")
+== equal => ===
 
-val = new Date()
-val.setDate(23)
-val.setFullYear(2023)
-console.log(val)
+!= not equal => !==
+*/
+/*
+let personID = 354;
+
+if(personID === undefined){
+    console.log("Id is undefined")
+} else {
+    console.log('Id is good')
+}*/
+
+/*
+const color = "blue"
+
+if(color === "red"){
+    console.log("Stop!")
+} else if (color === "yellow") {
+    console.log("Yellow!??!?")
+} else {
+    console.log("Error")
+}*/
+
+const name = "Kadi"
+const age = 40
+
+if(age > 0 && age <=12){
+    console.log(`${name} on laps`)
+}else if(age > 12 && age <18){
+    console.log(`${name} on nooruk`)
+}else{
+    console.log(`${name} on surmale lähedal`)
+}
+
+let id = 100
+
+// ? - ternary
+console.log(id === 100 ? "Ok" : "Not")
+
+// switch
+
+const color = "go fuck yourself"
+
+switch (color) {
+    case "red":
+        console.log("STOP")
+        break;
+    case "yellow":
+        console.log("Attention!")
+        break;
+    case "green":
+        console.log("Go!")
+        break;
+    default:
+        console.log("error")
+}
