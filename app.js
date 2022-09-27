ef8989affb06c0ee8fda4101e6d75fd3a48e7d82
Robin <robin.noormets@voco.ee>
2022-09-27 09:25:01 +0300

    DOM selectors for simple and multiply elements

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 let val;
 
-val = document
-val = document.all
-val = document.all[6]
-val = document.all.length
+val = document.getElementById('task')
+val = document.getElementById("task-title")
 
-val = document.head
-val = document.doctype
+/*const taskTitle =
+    document.getElementById("task-title")
+taskTitle.style.backgroundColor = "#333"
+taskTitle.style.color = "#fff"
+taskTitle.style.padding = "10px"
 
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
+taskTitle.textContent = "Defined Tasks"
+taskTitle.innerText = "My Favorite Tasks"
+taskTitle.innerHTML = "<b style='color: tomato'>My tasks</b>"
+console.log(taskTitle)*/
 
-val = document.forms
-val = document.forms[0]
-val = document.forms[0].method
-val = document.forms[0].action
+val = document.querySelector(".card-title")
+val = document.querySelector("ul")
+/*const task = document.querySelector("li")
+task.style.color = "red"*/
+val = document.querySelector("li:first-child")
+document.querySelector("li:nth-child(odd)").style.backgroundColor = "#ccc"
+document.querySelector("li:nth-child(even)").style.backgroundColor = "#f4f4f4"
 
-val = document.links
-val = document.links[1]
-val = document.links[1].className
-val = document.links[1].classList[1]
+// Multiply elements
 
-val = document.scripts
+val = document.querySelectorAll("li")
+oddList = document.querySelectorAll("li:nth-child(odd)")
+
+oddList.forEach((li) => {
+    li.style.background = "green"
+})
+
 console.log(val)
