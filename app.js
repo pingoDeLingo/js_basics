0390984e506073269f8b9fe5a2c1a6269528f821
Robin <robin.noormets@voco.ee>
2022-09-28 10:31:41 +0300

    Element creation

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
-let val;
+const li = document.createElement("li")
+li.className = "collection-item"
+let text = document.createTextNode("Study element creation")
+li.appendChild(text)
+
+const a = document.createElement("a")
+a.className = "blue-text text-darken 2 secondary-content"
+text = document.createTextNode("X")
+a.appendChild(text)
+
+li.appendChild(a)
 
 const list = document.querySelector("ul")
-const listitem = document.querySelector("li")
+list.appendChild(li)
 
-/*console.log(listitem)*/
-
-val = list
-val = list.childNodes
-val = list.childNodes[1]
-val = list.childNodes[1].nodeName
-val = list.childNodes[1].nodeType
-
-val = list.children
-val = list.children[1].children
-
-val = list.firstChild
-val = list.firstElementChild
-
-val = list.lastChild
-val = list.lastElementChild
-
-val = list.childElementCount
-
-val = listitem.parentNode
-val = listitem.parentElement
-val = listitem.parentElement.parentElement
-
-val = listitem.nextSibling
-val = listitem.nextElementSibling
-val = listitem.nextElementSibling.nextElementSibling.previousElementSibling
-console.log(val)
+console.log(li)
